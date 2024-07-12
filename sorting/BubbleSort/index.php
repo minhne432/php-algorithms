08bcfc63574376efdc9e2213922c9c8b09a50012
minh <nh.minh0403@gmail.com>
2024-07-12 14:34:31 +0700

    bubbleSort + 4

--- sorting/BubbleSort/index.php
+++ sorting/BubbleSort/index.php
@@ -1,6 +1,24 @@
-
-
 <?php
-$a = array_fill(5, 6, 'banana');
-print_r($a);
-?>
+
+function bubbleSort(array $array): array
+{
+  $n = count($array);
+
+  for ($i = 0; $i < $n - 1; $i++) {
+    for ($j = 0; $j < $n - $i - 1; $j++) {
+      if ($array[$j] > $array[$j + 1]) {
+        $temp = $array[$j];
+        $array[$j] = $array[$j + 1];
+        $array[$j + 1] = $temp;
+      }
+    }
+  }
+
+  return $array;
+}
+
+$array = [64, 34, 25, 12, 22, 11, 90];
+
+$sortedArray = bubbleSort($array);
+
+print_r($sortedArray);
