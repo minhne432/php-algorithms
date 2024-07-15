cb109ae0c9feaae508a3c53284676f97403a7d35
minh <nh.minh0403@gmail.com>
2024-07-15 11:05:40 +0700

    merge sort +2

--- sorting/mergeSort/index.php
+++ sorting/mergeSort/index.php
@@ -1,12 +1,11 @@
 <?php
-// Merge function to merge two halves
+
 function merge(array $left, array $right): array
 {
   $result = [];
   $i = 0;
   $j = 0;
 
-  // Compare each element and merge into result array
   while ($i < count($left) && $j < count($right)) {
     if ($left[$i] <= $right[$j]) {
       $result[] = $left[$i];
@@ -17,9 +16,8 @@ function merge(array $left, array $right): array
     }
   }
 
-  // Append remaining elements, if any
   while ($i < count($left)) {
-    $result[] = $left[$i];
+    $results[] = $left[$i];
     $i++;
   }
 
@@ -31,28 +29,22 @@ function merge(array $left, array $right): array
   return $result;
 }
 
-// Merge sort function
+
 function mergeSort(array $array): array
 {
-  // Base case: if the array has 1 or 0 elements, it's already sorted
   if (count($array) <= 1) {
     return $array;
   }
 
-  // Divide the array into halves
   $middle = intdiv(count($array), 2);
   $left = array_slice($array, 0, $middle);
   $right = array_slice($array, $middle);
 
-  // Recursively sort each half and merge them
   return merge(mergeSort($left), mergeSort($right));
 }
 
-// Sample array
 $array = [38, 27, 43, 3, 9, 82, 10];
 
-// Calling the mergeSort function
 $sortedArray = mergeSort($array);
 
-// Printing the sorted array
 print_r($sortedArray);
